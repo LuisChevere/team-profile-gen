57b24dbadc6f9563f5f7c2112b554d907ecbe671
Luis <Hairchevere@gmail.com>
2022-12-15 15:29:06 -0600

    finished engineer test

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,14 +1,20 @@
 const Engineer = require('../lib/engineers')
 
 test('creates engineer object', () => {
-    const engineer = new Engineer('Luis', 34, 'hairchevere@gmail.com');
+    const engineer = new Engineer('Luis', 34, 'hairchevere@gmail.com', 'LuisChevere');
 
     expect(engineer.github).toEqual(expect.any(String));
 })
 
 test('gets engineer github', () => {
-    const engineer = new Engineer('Luis', 34, 'hairchevere@gmail.com');
+    const engineer = new Engineer('Luis', 34, 'hairchevere@gmail.com', 'LuisChevere');
 
     expect(engineer.getGithub().toEqual(expect.stringContainer(engineer.github.toString())));
 
 });
+
+test('gets role of employee', () => {
+    const engineer = new Engineer('Luis', 34, 'hairchevere@gmail.com', 'LuisChevere');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+})
