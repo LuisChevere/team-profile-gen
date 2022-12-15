787b185ffc1a7daa476ffe213fc0463731ff44db
Luis <Hairchevere@gmail.com>
2022-12-15 12:57:20 -0600

    started employee test

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,1 +1,9 @@
-const Employee = require('..')
+const Employee = require('../lib/employees')
+
+test('creates employee object', () => {
+    const employee = new Employee('Luis', 34, 'hairchevere@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
