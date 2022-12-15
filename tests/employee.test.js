ded4c0d46e1ec1bc1dbb20dab68f918dfec1bf02
Luis <Hairchevere@gmail.com>
2022-12-15 13:38:58 -0600

    finished test for employee

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -7,3 +7,27 @@ test('creates employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+test('gets employee name', () => {
+    const employee = new Employee('Luis', 34, 'hairchevere@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('gets employee ID', () => {
+    const employee = new Employee('Luis', 34, 'hairchevere@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('gets employee email', () => {
+    const employee = new Employee('Luis', 34, 'hairchevere@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+test('gets role of employee', () => {
+    const employee = new Employee('Luis', 34, 'hairchevere@gmail.com');
+
+    expect(employee.getRole()).toEqual("Employee");
+});
