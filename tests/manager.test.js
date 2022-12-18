2e8bf722ed2aef5cd6d5077f8c2459d798cb969d
Luis <Hairchevere@gmail.com>
2022-12-18 15:15:42 -0600

    fixed const information

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,13 +1,13 @@
 const Manager = require('../lib/managers');
 
 test('create Manager object', () => {
-    const manager = new Manager('Luis', 34, 'hairchevere@gmail.com', 'LuisChevere', 4);
+    const manager = new Manager('Luis', 34, 'hairchevere@gmail.com', 4);
 
     expect(manager.officeNumber).toEqual(expect.any(Number));
 }) 
 
 test('gets role of employee', () => {
-    const manager = new Manager('Luis', 34, 'hairchevere@gmail.com', 'LuisChevere', 4);
+    const manager = new Manager('Luis', 34, 'hairchevere@gmail.com');
 
     expect(manager.getRole().toEqual("Manager"));
 })
