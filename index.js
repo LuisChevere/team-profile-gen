915d4fda4bb047eb4684d8f4f7d638e2e94077f8
Luis <Hairchevere@gmail.com>
2022-12-18 14:24:51 -0600

    added function for manager, engineer, intern

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Intern = require('./lib/interns');
 const fs = require('fs');
 const inquirer = require('inquirer');
 const { type } = require('os');
+const { default: Choice } = require('inquirer/lib/objects/choice');
 const teamArray = [];
 
 const addManager = () => {
@@ -72,3 +73,84 @@ const addManager = () => {
         console.log(manager);
     })
 };
+
+const addEmployee = () => {
+    console.log(`Adding employees to Team`);
+
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Please choose employee's role.",
+            Choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "what is your employee's name?",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter employee's name.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter employee ID.",
+            validate: nameInput => {
+                if(isNaN(nameInput)) {
+                    console.log("Please enter employee ID.")
+                    return false;
+                } else {
+                    return true;
+                }
+                 
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter employee email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if(valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter the email.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Please enter employee github username.",
+            when: (input) => input.role === "Engineer",
+            Validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter employee github name.")
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Please enter intern's school.",
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter intern's school.")
+                }
+            }
+        },
+        
+    ])
+}
