954937873b9d68ef8ea5e5e0665b9bbf74c8b347
Luis <Hairchevere@gmail.com>
2022-12-18 12:59:55 -0600

    finished intern test

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -4,4 +4,16 @@ Test('creates Intern object', () => {
     const intern = new Intern('Luis', 34, 'hairchevere@gmail.com', 'LuisChevere', 'UTSA');
 
     expect(intern.school).toEqual(expect.any(String));
+});
+
+test('gets employee school', () => {
+    const intern = new Intern('Luis', 34, 'hairchevere@gmail.com', 'LuisChevere', 'UTSA');
+
+    expect(intern.getSchool()).toEqual(expect.stringContainer(intern.school.toString()));
+});
+
+test('gets role of employee', () => {
+    const intern = new Intern('Luis', 34, 'hairchevere@gmail.com', 'LuisChevere', 'UTSA');
+
+    expect(intern.getRole()).toEqual("Intern");
 })
