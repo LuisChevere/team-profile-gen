7c1b07443bbe9e908b72ee5ab5c712de5e84c634
Luis <Hairchevere@gmail.com>
2022-12-20 15:36:36 -0600

    fixed intern email

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -48,8 +48,8 @@ const generateIntern = function(intern) {
             
             <div class="card-body">
                 <p class="id">ID: ${intern.id}</p>
-                <p class="email">Email: <a href:"mailto:${intern.email}">${intern.email}</a></p>
-                <p class="school">School: ${intern.school}></p>
+                <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
             </div>
         </div>
     </div>
