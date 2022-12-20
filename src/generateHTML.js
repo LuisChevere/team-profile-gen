232e926be34e114e301a030404090b420ee34c8a
Luis <Hairchevere@gmail.com>
2022-12-20 15:29:18 -0600

    added missing <

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -49,7 +49,7 @@ const generateIntern = function(intern) {
             <div class="card-body">
                 <p class="id">ID: ${intern.id}</p>
                 <p class="email">Email: <a href:"mailto:${intern.email}">${intern.email}</a></p>
-                <p class="school">School: ${intern.school}>/p>
+                <p class="school">School: ${intern.school}></p>
             </div>
         </div>
     </div>
